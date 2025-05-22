df4cb1697f108477d7b279b3506cafefe0481d0b
youngwoonglee <9902abcd@gmail.com>
2025-05-22 17:30:42 +0900

    remove sg

--- init.lua
+++ init.lua
@@ -172,13 +172,6 @@ require('lazy').setup({
     },
   },
 
-  {
-    'sourcegraph/sg.nvim',
-    event = 'VimEnter',
-    config = function()
-      require('sg').setup {}
-    end,
-  },
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle
